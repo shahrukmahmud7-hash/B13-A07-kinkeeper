1dcfa9ee95f91adbfd336430d84b4842d21297e7
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-04-14 20:38:03 +0600

    banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home page</h2>
-        </div>
+            <div>
+      {/* Banner */}
+      <div className="text-center mb-10 mt-20">
+        <h1 className="text-3xl font-bold">
+          Friends to keep close in your life
+        </h1>
+        <p className="text-gray-500 mt-2">
+          Track and manage your relationships easily
+        </p>
+        <button className="mt-4 bg-[#244D3F] text-white px-4 py-2 rounded">
+          + Add Friend
+        </button>
+      </div>
+
+    
+    </div>
     );
 };
 
-export default Home;
+export default Home; 
