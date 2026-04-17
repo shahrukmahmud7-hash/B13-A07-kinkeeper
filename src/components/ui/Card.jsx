c5cc45bbc924663d64267f60592cadc17a813ebb
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-04-17 18:05:27 +0600

    tags and status fixed

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -12,7 +12,8 @@ const Card = ({ friend }) => {
       onClick={() => navigate(`/friend/${friend.id}`)}
       className="card w-64 bg-base-100 border
       border-gray-100 shadow-[0_8px_30px_rgb(0,0,0,0.04)]
-      p-6 flex flex-col items-center cursor-pointer hover:scale-105 transition"
+      p-6 flex flex-col items-center
+      cursor-pointer hover:scale-105 transition"
     >
       <div className="avatar mb-4">
         <div className="w-24 rounded-full">
@@ -30,14 +31,20 @@ const Card = ({ friend }) => {
       </div>
 
       <div className="mt-4 flex flex-col items-center gap-2">
+
+        <div className='flex gap-2 '>
+           {friend.tags.map(tag => (
         
-        {/* TAG FIX */}
-        <div className="bg-emerald-100 text-emerald-700 px-4 py-2 text-xs rounded">
-          {friend.tags.join(", ")}
+        <div className="bg-emerald-100 text-emerald-700
+         px-4 py-2 text-xs rounded-3xl">
+          {tag}
+        </div>
+       ))}
         </div>
 
-        {/* STATUS */}
-        <div className="badge badge-lg bg-orange-400 text-white px-6 py-2 rounded-full">
+        <div className={`badge badge-lg text-white px-6 py-2 rounded-full
+           ${friend.status==="On-Track" ? "bg-[#244D3F]" : 
+           friend.status === "Overdue" ? "bg-[#EF4444]" : "bg-[#EFAD44]"}`}> 
           {friend.status}
         </div>
 
@@ -46,4 +53,4 @@ const Card = ({ friend }) => {
     );
 };
 
-export default Card;
+export default Card; 
